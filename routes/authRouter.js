44cccbf915a429788dd682e99b39e11bf9362818
ViDA_TG <victorademiju.t@gmail.com>
2023-01-27 21:56:05 +0100

    Joi validation

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,14 +2,16 @@ const express = require('express')
 const router = express.Router()
 const createError = require('http-errors')
 const bcrypt = require('bcrypt')
+
+const  { authSchema } = require('../validationSchema')
 const User = require('../models/UserModel')
 
 router.post('/register', async (req, res, next) => {
     try{
         const { email, password } = req.body
-        if(!email || !password) res.send("NOT HAPPYYYYYYYY")
+        const result = await authSchema.validateAsync(req.body)
         const userExists = await User.findOne({email})
-        if (!userExists) {
+        if (userExists) {
             const hashedPassword = bcrypt.hash(password, 10)
             const newUser = new User({email, hashedPassword})
             const savedUser = await newUser.save()
