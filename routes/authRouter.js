e301c8ecfe343b7b6d046518d1ede2bcff40474d
ViDA_TG <victorademiju.t@gmail.com>
2023-01-28 03:22:57 +0100

    Auth - registration

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -10,15 +10,13 @@ router.post('/register', async (req, res, next) => {
     try{
         const { email, password } = req.body
         const result = await authSchema.validateAsync({ email, password })
-        res.send(result)
-
-        /*
+        
         const userExists = await User.findOne({email})
         if (userExists) {
             const hashedPassword = bcrypt.hash(password, 10)
             const newUser = new User({email, hashedPassword})
             const savedUser = await newUser.save()
-        } */
+        }
     }catch(error){
         return next(createError(200, error.message))
     }
