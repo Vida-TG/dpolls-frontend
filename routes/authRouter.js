b4966ee529004cfe7500e0da7edf482f3301f740
ViDA_TG <victorademiju.t@gmail.com>
2023-01-28 03:07:27 +0100

    Learning http-errors

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -19,7 +19,7 @@ router.post('/register', async (req, res, next) => {
             const savedUser = await newUser.save()
         } */
     }catch(error){
-        throw createError(400, error.message)
+        next(createError(400, error.message))
     }
 })
 
