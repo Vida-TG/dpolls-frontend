be27949b6f8903417abe5dfff46394ee992633b6
Vida-TG <victorademiju.t@gmail.com>
2023-01-29 20:35:38 +0100

    Registration

--- jwtToken.js
+++ jwtToken.js
@@ -5,12 +5,12 @@ module.exports = {
     signAccessToken: (userID) => {
         return new Promise((resolve, reject) => {
             const payload = {
-                name: "Human ing"
+                name: "Human"
             }
-            const secret = "jfjwjojjbsrgteewgewtauthtoken4fi4f"
+            const secret = "jfjwjojjbsrgtehtoken4fi4f"
             const options = {
-                expiresIn: '1d',
-                issuer: 'https://package.com',
+                expiresIn: '1h',
+                issuer: 'www.package.com',
                 audience: userID,
             }
             JWT.sign(payload, secret, options, (err, token) => {
