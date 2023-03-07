73509d3b4bb8fad47a5e090edef83f828a6ef2b5
Hardik.eth.sol <hardikazmi.gmail.com>
2023-03-07 06:52:40 +0100

    Restructure

--- jwtToken.js
+++ jwtToken.js
@@ -5,12 +5,12 @@ module.exports = {
     signAccessToken: (userID) => {
         return new Promise((resolve, reject) => {
             const payload = {
-                name: "Human"
+                name: "HumanII"
             }
-            const secret = "ghnumu jigugunurr  fgryfbfhfjfdrhdjjtttttfbee----------------------------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"
+            const secret = "aswefrgtbrvrb rgegtegr rge5tg4wretrdjjtttttfbee----------------------------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"
             const options = {
                 expiresIn: '1h',
-                issuer: 'https://www.package.com',
+                issuer: 'https://www.packagedd.com',
                 audience: userID,
             }
             JWT.sign(payload, secret, options, (err, token) => {
